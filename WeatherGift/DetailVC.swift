d5fc73a96f25c1ead2a59b5a178f71e089224484
Carter Borchetta <carterborch@me.com>
2019-10-10 21:25:59 -0400

    7.2 Complete

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Carter Borchetta on 10/10/19.
+//  Copyright © 2019 Carter Borchetta. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+
+}
